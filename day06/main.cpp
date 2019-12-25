0d08f22eff84a82ef3d430719b191408bfb31b1e
gahgsp <kelvinbianchi@gmail.com>
2019-12-25 11:59:07 -0300

    Finished second part of day 6

--- day06/main.cpp
+++ day06/main.cpp
@@ -17,8 +17,62 @@ int countOrbits(std::string planetName, int totalOrbits) {
     return totalOrbits;
 }
 
+std::vector<std::string> calculatePathToBeginning(std::string origPlanet, std::vector<std::string> currPath) {
+    if (orbits.find(origPlanet) != orbits.end()) {
+        currPath.push_back(origPlanet);
+        return calculatePathToBeginning(orbits[origPlanet], currPath);
+    }
+    return currPath;
+}
+
+std::string findCommonAncestor(std::vector<std::string> originPath, std::vector<std::string> destinationPath) {
+    std::string commonAncestor;
+    for (const std::string& oPlanetStep : originPath) {
+        bool foundCommonAncestor = false;
+        for (const std::string& dPlanetStep : destinationPath) {
+            if (oPlanetStep == dPlanetStep) {
+                commonAncestor = oPlanetStep;
+                foundCommonAncestor = true;
+                break;
+            }
+        }
+        if (foundCommonAncestor) {
+            break;
+        }
+    }
+    return commonAncestor;
+}
+
+int calculateStepsToSanta() {
+    std::vector<std::string> origPlanetPath;
+    origPlanetPath = calculatePathToBeginning("YOU", origPlanetPath);
+    std::vector<std::string> destPlanetPath;
+    destPlanetPath = calculatePathToBeginning("SAN", destPlanetPath);
+
+    std::string commonAncestor = findCommonAncestor(origPlanetPath, destPlanetPath);
+
+    int stepsFromOrigin = 0;
+    for (int i = 0; i < origPlanetPath.size(); i++) {
+        if (origPlanetPath[i] != commonAncestor) {
+            stepsFromOrigin++;
+        } else {
+            break;
+        }
+    }
+
+    int stepsFromDest = 0;
+    for (int i = 0; i < destPlanetPath.size(); i++) {
+        if (destPlanetPath[i] != commonAncestor) {
+            stepsFromDest++;
+        } else {
+            break;
+        }
+    }
+    return (stepsFromOrigin + stepsFromDest) - 2;  // Can't considerer both origin and destination
+}
+
 int main() {
-    std::ifstream file(R"(C:\Web Projects\C++\advent-of-code-2019\day06\input.txt)");
+    std::ifstream file("input.txt");
     std::string line;
 
     while(getline(file, line, '\n')) {
@@ -36,6 +90,8 @@ int main() {
     }
 
     std::cout << "The result of the first part of the puzzle is: " << totalOrbits << std::endl;
+
+    std::cout << "The result of the second part of the puzzle is: " << calculateStepsToSanta() << std::endl;
 
     return 0;
 }
